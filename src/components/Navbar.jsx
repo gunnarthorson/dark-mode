6919841d0f74af8d55f0a7dd35ca2bd249cb9f5e
fluxlog <fluxlog@hotmail.com>
2020-03-11 17:17:32 -0700

    mvp plus links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useDarkMode from './hooks/useDarkMode'
+import { Dropdown, Menu } from 'semantic-ui-react'
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
@@ -9,6 +11,27 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
+      <Menu>
+    <Menu.Item href="https://www.coingecko.com/en">Home</Menu.Item>
+    <Dropdown text='Information' pointing className='link item'>
+      <Dropdown.Menu>
+        <Dropdown.Header>Exchanges</Dropdown.Header>
+        <Dropdown.Item>
+        </Dropdown.Item>
+        <Dropdown.Item href="https://www.coingecko.com/en/exchanges">Spot</Dropdown.Item>
+        <Dropdown.Item href="https://www.coingecko.com/en/exchanges/derivatives">Derivatives</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Header>Resources</Dropdown.Header>
+        <Dropdown.Item href="https://www.coingecko.com/en/api">API</Dropdown.Item>
+        <Dropdown.Item href="https://newsletter.coingecko.com/landing/subscribe">Newsletter</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    <Menu.Item href="https://bitcointalk.org/index.php?topic=561516.0">Forums</Menu.Item>
+    <Menu.Item href="https://support.coingecko.com/help_center">Help</Menu.Item>
+  </Menu>
+
+
+      
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
